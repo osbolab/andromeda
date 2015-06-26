1d63c1a58757c15d58b37636e4d2bc835655d869
Matt Barnard <msbarnar@gmail.com>
2015-06-25 22:25:17 -0700

    import project

---
+++ server/src/main/java/com/andromeda/server/Main.java
@@ -0,0 +1,12 @@
+package com.andromeda.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  public static final Logger log = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+
+  }
+}
