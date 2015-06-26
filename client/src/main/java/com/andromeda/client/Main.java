a43cd3ebfc0a8e51c7efa8a1bdb5ca93249c4cda
Matt Barnard <msbarnar@gmail.com>
2015-06-26 01:21:23 -0700

    Add Lightweight Java Games Library dependency to client

--- client/src/main/java/com/andromeda/client/Main.java
+++ client/src/main/java/com/andromeda/client/Main.java
@@ -1,26 +1,6 @@
 package com.andromeda.client;
 
-import java.net.InetSocketAddress;
-import java.util.Scanner;
-
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Inbox;
-import akka.actor.Props;
-
 public class Main {
   public static void main(String[] args) {
-    final InetSocketAddress remote = new InetSocketAddress("localhost", 9999);
-
-    final ActorSystem system = ActorSystem.create("andromeda-client");
-    final Inbox inbox = Inbox.create(system);
-
-    final ActorRef handler = system.actorOf(Props.create(SimpleHandler.class), "message-handler");
-    final ActorRef client = system.actorOf(Client.to(remote, handler), "client");
-
-    Scanner scanner = new Scanner(System.in);
-    scanner.nextLine();
-
-    system.shutdown();
   }
 }
