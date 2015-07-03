0a4deb64befead7d11a0d16dc9dc60f75ddc9da4
Matt Barnard <msbarnar@gmail.com>
2015-07-02 23:06:48 -0700

    after throwing out (almost all of) game.oldmap package

--- server/src/main/java/com/andromeda/game/oldmap/MapGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.andromeda.game.oldmap;
-
-
-public interface MapGenerator<T> {
-  Tile<T> toLayer(int x, int y, int layer);
-  <R extends TileRequest> void getDependencyTree(R request);
-}
